bc6865768ad4e8e2401c972e40c653a2cbbd7f66
beesou777 <sbeesou@gmail.com>
2023-12-09 10:25:48 +0545

    first time with tosa

---
+++ @types/user.ts
@@ -0,0 +1,12 @@
+interface User {
+    store_name:String,
+    first_name:String,
+    last_name:String,
+    profile_picture:String,
+    email:String,
+    role:String,
+    sub_domain:string,
+    password:String,
+    phone_number:Number,
+    username:String
+}
